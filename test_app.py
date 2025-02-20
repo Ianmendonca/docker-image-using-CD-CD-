61c563f86b09bb6968f0d694446b40bebd407b74
Ianmendonca <fransonmendonca1996@gmail.com>
2025-02-20 18:08:19 +0100

    added test message

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import app
 
 def test_home():
     response=app.test_client().get("/")
-
+    print('inside test')
     assert response.status_code==200
     assert response.data=="hello world"
