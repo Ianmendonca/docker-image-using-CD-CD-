c4b2feadb399880415856b1cb17b42da64eb81d5
Ianmendonca <fransonmendonca1996@gmail.com>
2025-02-20 18:29:14 +0100

    new line

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_home():
     response=app.test_client().get("/")
     print('inside test')
     assert response.status_code==200
-    assert response.data=='hello world'
+    assert response.data==b'hello world'
